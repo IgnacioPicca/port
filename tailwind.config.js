5825519753736342812baf6f97c769dc85c5e253
Ignacio Picca <piccaignacio@gmail.com>
2023-04-13 18:40:13 -0300

    style

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,21 +5,34 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "deep-blue": "#010026",
-        blue: "#893587", 
-        red: "#c56fc1",
-        yellow: "#ff99fb",
-        purple: "#8d43ff",
-        grey: "#ededed",
-        "dark-grey": "#757575",
-        "opaque-black": "rgba(0,0,0,0.35)",
+        dominant: "#B7A2D7", 
+        greyl: "#B0A8B9",
+        greyd: "#7C7484",
+        violetl: "#9A89B4",
+        redl: "#FA0000",
+        redd: "#B70000",
+        bluel: "#80B2C9",
+        light: "#FDF7FF",
+        dark: "#1E1926",
+        hover: "#362B48",
+        
+        blue: "#0A1828",
+        turquoise: "#178582",
+        gold: "#BFA181",
+        white: "#FDF7FF",
+        black: "#000000",
+        color1: "#005351",
+        color2: "#003B3A",
+        color3: "#002524",
+
+
       },
       backgroundImage: (theme) => ({
         "gradient-rainbow":
-          "linear-gradient(81.66deg, #00B5EE 7.21%, #FF45A4 45.05%, #FFBA00 78.07%)",
+          "linear-gradient(81.66deg, #BFA181 7.21%, #487269 45.05%, #2F4858 78.07%)",
 
         "gradient-rainblue":
-          "linear-gradient(90deg, #24CBFF 14.53%, #FC59FF 69.36%, #FFBD0C 117.73%)",
+          "linear-gradient(81.66deg, #BFA181 7.21%, #487269 45.05%, #2F4858 78.07%)",
       }),
       fontFamily: {
         playfair: ["Playfair Display", "serif"],
